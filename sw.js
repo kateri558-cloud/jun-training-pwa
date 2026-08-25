d770e0db541a8ff7e5b0d1f972b8670ca1636f1a
kateri558-cloud <kateri558@gmail.com>
2026-08-26 05:46:49 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='jun-training-v4';
+const CACHE='jun-training-v5';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
